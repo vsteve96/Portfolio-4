34dbb5058401f8feb7fb93c50c7318b81d824d91
Istvan Vizhanyo <vizhanyo.istvan0@gmail.com>
2024-04-19 16:28:07 +0000

    Prepared for Heroku deployment

--- about/models.py
+++ about/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
--- book_blog/book_blog/urls.py
+++ book_blog/book_blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_project project.
+URL configuration for book_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
-from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
-    path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
